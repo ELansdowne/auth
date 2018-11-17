b3a996f313c4197babfee7b70b362cd0e27e0d22
ELansdowne <elansdowneuk@gmail.com>
2018-11-17 12:28:22 +0530

    AUTH [feature] lint and foldr setup added

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyDkpjZPs6BFyXGc9HsBFNhBS_68NLzeKpg",
+  authDomain: "kauthik-5f02b.firebaseapp.com",
+  databaseURL: "https://kauthik-5f02b.firebaseio.com",
+  projectId: "kauthik-5f02b",
+  storageBucket: "kauthik-5f02b.appspot.com",
+  messagingSenderId: "405797528802"
+};
+
+export default config;
